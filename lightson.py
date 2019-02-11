6e533c657e9f829ecd9e53c6008834c2ddd6cd7e
robinveitch <47534294+robinveitch@users.noreply.github.com>
2019-02-11 17:37:15 +0000

    Update lightson.py

--- lightson.py
+++ lightson.py
@@ -5,8 +5,10 @@ import time
 while True:
     for i in range(8):
         blinkt.set_pixel(i,250,80,250)
+    blinkt.show()
     time.sleep(1)
     for i in range(8):
         blinkt.set_pixel(i,250,250,50)
+    blinkt.show()
     time.sleep(1)
     
